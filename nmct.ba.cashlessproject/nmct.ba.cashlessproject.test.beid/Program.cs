5b1d2507a885d9e4847cbc14ed80cf5ea45a5fd6
Anthony Madhvani <anthony@madhvani.be>
2015-01-03 15:04:25 +0100

    BeID Tests

---
+++ nmct.ba.cashlessproject/nmct.ba.cashlessproject.test.beid/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swelio.Engine;
+
+namespace nmct.ba.cashlessproject.test.beid
+{
+    class Program
+    {
+        //Post-build event:
+        //copy /Y "$(SolutionDir)dll\*.dll" "$(TargetDir)"
+
+        static void Main(string[] args)
+        {
+            Card card = null;
+
+            Manager engine = new Manager();
+            engine.Active = true;
+            CardReader reader = engine.GetReader(0);
+            if (reader != null)
+            {
+                reader.ActivateCard();
+                card = reader.GetCard();
+                if (card != null)
+                {
+                    Identity identity = card.ReadIdentity();
+                    if (identity != null)
+                    {
+                        Console.WriteLine("{0}", identity.NationalNumber);
+                    }
+                }
+                reader.DeactivateCard();
+            }
+            Console.ReadLine();
+            card = reader.GetCard();
+            if (reader.CardPresent)
+                card = reader.GetCard();
+
+            engine.Dispose();
+        }
+    }
+}
